f6efa787bb6b41a6b07c76cdd079881b126bf6ff
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-04-23 16:53:44 -0400

    passed all tests

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -2,15 +2,36 @@ class LinkedList:
     """
     Put docstring here
     """
-
+    #
     def __init__(self):
         # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        current = self.head
+        result = ""
+        while current:
+            result += f"{{ {str(current.value)} }} -> "
+            current = current.next
+        return result + "NULL"
 
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+
+    def includes(self, target_value):
+        current = self.head
+        while current:
+            if current.value == target_value:
+                return True
+            current = current.next
+        return False
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 class TargetError:
     pass

b3fdcc5c3cfc0dce56371e60b43674a89551f838
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-04-22 20:51:47 -0400

    completed some tests for singly linked lists

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -2,15 +2,31 @@ class LinkedList:
     """
     Put docstring here
     """
-
+    #
     def __init__(self):
         # initialization here
+        self.head = None
+
+    def insert(self, value):
+        self.head = Node(value)
+
+
+
+    def to_string(self):
         pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def includes(self, target_value):
+        current = self.head
+        while current:
+            if current.value == target_value:
+                return True
+            current = current.next
+        return False
 
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next_node = None
 
 class TargetError:
     pass

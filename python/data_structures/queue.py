35584a622ea8453dedf26d23519114df2e4d6418
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-04-29 21:27:08 -0400

    passed 12 tests

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,53 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+from data_structures.linked_list import Node
+
+
 class Queue:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.rear = None
+        self.front = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        if not self.front:
+            self.front = Node(value)
+            self.rear = self.front
+            return
+            # return self.front.value
+        temp = self.rear
+        self.rear = Node(value)
+        temp.next = self.rear
+        return
+
+
+
+
+        # elif self.front:
+        #     self.rear.next
+
+    def dequeue(self):
+        if not self.front:
+            raise InvalidOperationError
+        old_front = self.front
+        self.front = old_front.next
+        old_front.next = None
+        return old_front.value
+
+    def is_empty(self):
+        if self.front is None:
+            return True
+        return False
+
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError
+        return self.front.value
+
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next

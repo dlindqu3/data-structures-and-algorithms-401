f187b95f9751848b1293e329f8558323a6c3f40b
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-05-04 21:07:26 -0400

    passed tests cc13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,25 @@
-from data_structures.queue import Queue
+# from data_structures.queue import Queue
+from data_structures.stack import Stack
 
+def multi_bracket_validation(str):
+    if len(str) %2 != 0:
+        return False
+    s = Stack()
+    openers = ['(', '[', '{']
+    closers = [')', ']', '}']
+    matches = {'(': ')', '[': ']', '{': '}'}
+    for char in str:
+        if char in openers:
+            s.push(char)
+        elif char in closers:
+            if s.is_empty():
+                return False
+            elif matches[s.peek()] == char:
+                s.pop()
+            else:
+                return False
+    return s.is_empty()
 
-def multi_bracket_validation():
-    pass
+#methods: peek, push, pop, is_empty, top
+#returns a bool
+#bool shows whether or not brackets in str are balanced

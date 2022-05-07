675ff8e7f9f4f70816001eb6cbe6342f0288fee8
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-05-06 20:46:05 -0400

    passed half of tests

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,51 @@
 class BinaryTree:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        ordered_values = []
+        def walk(root, values):
+            if not root:
+                return
+            # Task 1: do something
+            values.append(root.value)
+            # Task 2: go left
+            walk(root.left, values)
+            # Task 3: go right
+            walk(root.right, values)
+        walk(self.root, ordered_values)
+        return ordered_values
 
+    def in_order(self):
+        ordered_values = []
+        def walk(root, values):
+            if not root:
+                return
+            # Task 1: go left
+            walk(root.left, values)
+            # Task 2: do something
+            values.append(root.value)
+            # Task 3: go right
+            walk(root.right, values)
+        walk(self.root, ordered_values)
+        return ordered_values
+
+    def post_order(self):
+        ordered_values = []
+        def walk(root, values):
+            if not root:
+                return
+            # Task 1: go left
+            walk(root.left, values)
+            # Task 2: go right
+            walk(root.right, values)
+            # Task 3: do something
+            values.append(root.value)
+        walk(self.root, ordered_values)
+        return ordered_values
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
